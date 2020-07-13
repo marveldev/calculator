98bedc63599c8236c290a2c4e68047d76282f6bb
Marvel Dev <marvellousalika48@gmail.com>
2020-07-13 19:27:40 +0100

    fix bug with backspace button
    - cleared console
    - fix bug with symbol

--- index.js
+++ index.js
@@ -10,30 +10,14 @@ function appendToOutput(value) {
     secondValue += value
   }
   output.innerHTML += value;
-  console.log('firstvalue:', firstValue);
-  console.log('secondvalue:', secondValue);
-}
-
-function calculate(currentSymbol) {
-  if (secondValue) {
-    displayResult();
-    firstValue = parseFloat(firstValue); 
-    output.innerHTML = firstValue;
-  } else {
-    firstValue = parseFloat(firstValue); 
-    symbol = currentSymbol;
-    output.innerHTML += symbol;
-  }
 }
 
 function displayResult() {
   switch(symbol) {
     case '+':
       firstValue = firstValue + parseFloat(secondValue);
-      firstValue = firstValue.toString();
       secondValue = '';
-      output.innerHTML = firstValue;
-      console.log( 'type of one', typeof firstValue);
+      output.innerHTML = firstValue.toString();
     break;
     case '-':
       firstValue = firstValue - parseFloat(secondValue);
@@ -46,13 +30,26 @@ function displayResult() {
       firstValue = firstValue.toString();
       secondValue = '';
       output.innerHTML = firstValue;
-      break;
+    break;
     case '÷':
       firstValue = firstValue / parseFloat(secondValue);
       firstValue = firstValue.toString();
       secondValue = '';
       output.innerHTML = firstValue;
-      break;
+    break;
+  }
+}
+
+function calculate(currentSymbol) {
+  if (secondValue) {
+    displayResult();
+    firstValue = parseFloat(firstValue); 
+    symbol = currentSymbol;
+    output.innerHTML = firstValue + symbol;
+  } else {
+    firstValue = parseFloat(firstValue); 
+    symbol = currentSymbol;
+    output.innerHTML = firstValue + symbol;
   }
 }
 
@@ -64,30 +61,22 @@ function clearOutput() {
 
 function deleteValue() {
   if (typeof(firstValue) == 'string') {
-    let arrayOutput = output.innerHTML.split(""); 
+    let arrayOutput = output.innerHTML.split(''); 
     arrayOutput.pop(); 
-    let newString = arrayOutput.join("");
+    let newString = arrayOutput.join('');
     firstValue = newString;
     output.innerHTML = firstValue;
-    console.log("newFirstValue:", firstValue);
   } else if (secondValue) {
-      let arrayOutput = secondValue.split(""); 
+      let arrayOutput = secondValue.split(''); 
       arrayOutput.pop();
-      let newString = arrayOutput.join(""); //ok
+      let newString = arrayOutput.join('');
       secondValue = newString;
       output.innerHTML = firstValue + symbol + secondValue;
-      console.log("newFirstValue:", firstValue);
-      console.log("newSecondValue:", secondValue);
-  } else if (symbol){
-      console.log(symbol);
-    
-      let arrayOutput = symbol.split('');
-      // arrayOutput.pop();
-      // arrayOutput.pop();
-      // let newString = arrayOutput.join("");
-      // firstValue = newString;
-      // output.innerHTML = newString ;
-      console.log(arrayOutput);
-      // console.log(firstValue);
+  } else {
+    let arrayOutput = output.innerHTML.split(''); 
+    arrayOutput.pop();
+    let newString = arrayOutput.join('');
+    firstValue = newString;
+    output.innerHTML = firstValue;
   }
 }
